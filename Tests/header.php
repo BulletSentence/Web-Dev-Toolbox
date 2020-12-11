cafe4f3fe0b7563baeb4b9e85b81c1bbb6f4c05b
Leonardo Almeida de Araújo <leonardo.a@acad.ifma.edu.br>
2020-12-11 11:43:51 -0300

    estilos

---
+++ Tests/header.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Sydney
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+ <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+<!-- ManyChat -->
+<script src="//widget.manychat.com/415657831904955.js" async="async"></script>
+<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) : ?>
+	<?php if ( get_theme_mod('site_favicon') ) : ?>
+		<link rel="shortcut icon" href="<?php echo esc_url(get_theme_mod('site_favicon')); ?>" />
+	<?php endif; ?>
+<?php endif; ?>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php do_action('sydney_before_site'); //Hooked: sydney_preloader() ?>
+
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'sydney' ); ?></a>
+
+	<?php do_action('sydney_before_header'); //Hooked: sydney_header_clone() ?>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-wrap">
+            <div class="container">
+                <div class="row">
+				<div class="col-md-2 col-sm-6 col-xs-12">
+		        <?php if ( get_theme_mod('site_logo') ) : ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo('name'); ?>"><img class="site-logo" src="<?php echo esc_url(get_theme_mod('site_logo')); ?>" alt="<?php bloginfo('name'); ?>" /></a>
+		        <?php else : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>	        
+		        <?php endif; ?>
+				</div>
+				<div class="col-md-8 col-sm-4 col-xs-12">
+					<div class="btn-menu"></div>
+					<nav id="mainnav" class="mainnav" role="navigation">
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'fallback_cb' => 'sydney_menu_fallback' ) ); ?>
+					</nav><!-- #site-navigation -->
+				</div>
+				<div class="col-md-2 col-sm-6 col-xs-12">
+                    <a href="https://neil197.typeform.com/to/GStj0x" style="margin-top: 10px;">
+                        <img alt="Get Started" src="/wp-content/uploads/2019/05/getstarted.png">
+                    </a>
+				</div>
+				</div>
+			</div>
+		</div>
+	</header><!-- #masthead -->
+
+	<?php do_action('sydney_after_header'); ?>
+
+	<div class="sydney-hero-area">
+		<?php sydney_slider_template(); ?>
+		<div class="header-image">
+			<?php sydney_header_overlay(); ?>
+			<img class="header-inner" src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>">
+		</div>
+		<?php sydney_header_video(); ?>
+
+		<?php do_action('sydney_inside_hero'); ?>
+		<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5000 20" preserveAspectRatio="xMidYMax slice" class="off-white-blue triangle-overlay">
+        <rect x="0" y="0" width="2480" height="100%"></rect>
+        <path d="M2480 0 l0 20 l20 0 l-20 -20"></path>
+        <path d="M2520 0 l0 20 l-20 0 l20 -20"></path>
+        <rect x="2520" y="0" width="2480" height="100%"></rect>
+      </svg>
+	</div>
+
+	<?php do_action('sydney_after_hero'); ?>
+
+	<div id="content" class="page-wrap">
+		<div class="container content-wrapper">
+			<div class="row">
